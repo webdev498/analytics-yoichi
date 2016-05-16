0a74d414522c95ab523031e7df4a22b5b9c28a1c
webdev498 <gcmobile@mail.com>
2016-05-16 14:23:34 +0530

    Moved getCountryIDByCountryCode function in utils.js and imported from their in WorldMap component and Removed all unnecessary utils functions for now

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 import Card from 'material-ui/lib/card/card';
 
-function getCountryIDByCountryCode(countryCode) {
-  const getCountryIDByCountryCode = {"AG":"01","BS":"02","BB":"03","BZ":"04","CA":"05","CR":"06","CU":"07","DM":"08","DO":"09","SV":"10","GD":"11","GT":"12","HT":"13","HN":"14","JM":"15","MX":"16","NI":"17","PA":"18","KN":"19","LC":"20","VC":"21","TT":"22","US":"23","GL":"24","AR":"25","BO":"26","BR":"27","CL":"28","CO":"29","EC":"30","FK":"31","GF":"32","GY":"33","PY":"34","PE":"35","SR":"36","UY":"37","VE":"38","DZ":"39","AO":"40","BJ":"41","BW":"42","BF":"43","BI":"44","CM":"45","CV":"46","CP":"47","TD":"48","KM":"49","CI":"50","CD":"51","DJ":"52","EG":"53","GQ":"54","ER":"55","ET":"56","GA":"57","GH":"58","GN":"59","GW":"60","KE":"61","LS":"62","LI":"63","LR":"64","MS":"65","MW":"66","ML":"67","MR":"68","MA":"69","MZ":"70","NA":"71","NE":"72","NG":"73","RW":"74","ST":"75","SN":"76","SC":"77","SL":"78","SO":"79","ZA":"80","SD":"81","SZ":"82","TZ":"83","TG":"84","TN":"85","UG":"86","WA":"87","ZM":"88","ZW":"89","GM":"90","CG":"91","MI":"92","AF":"93","AM":"94","AZ":"95","BD":"96","BT":"97","BN":"98","MM":"99","KH":"100","CN":"101","TP":"102","GE":"103","IN":"104","ID":"105","IA":"106","JP":"107","KZ":"108","KP":"109","KR":"110","KG":"111","LA":"112","MY":"113","MN":"114","NP":"115","PK":"116","PH":"117","RU":"118","SG":"119","LK":"120","TJ":"121","TH":"122","TM":"123","UZ":"124","VN":"125","TW":"126","HK":"127","MO":"128","AL":"129","AD":"130","AT":"131","BY":"132","BE":"133","BH":"134","BG":"135","HY":"136","CZ":"137","DK":"138","EE":"139","FI":"140","FR":"141","DE":"142","GR":"143","HU":"144","IS":"145","IR":"146","IT":"147","LV":"148","LN":"149","LT":"150","LU":"151","MK":"152","MT":"153","MV":"154","MC":"155","MG":"156","NL":"157","NO":"158","PL":"159","PT":"160","RO":"161","SM":"162","CS":"163","SK":"164","SI":"165","ES":"166","SE":"167","CH":"168","UA":"169","UK":"170","VA":"171","CY":"172","TK":"173","AU":"175","FJ":"176","KI":"177","MH":"178","FM":"179","NR":"180","NZ":"181","PW":"182","PG":"183","WS":"184","SB":"185","TO":"186","TV":"187","VU":"188","NC":"188","BA":"190","IZ":"191","IE":"192","JO":"193","KU":"194","LB":"195","OM":"196","QA":"197","SA":"198","SY":"199","AE":"200","YM":"201","PR":"202","KY":"203","SS":"204","KO":"205"};
-
-  const countryID = getCountryIDByCountryCode[countryCode];
-  return countryID;
-}
+import {getCountryIDByCountryCode} from 'utils/utils';
 
 function getData(dataSource) {
   const data = dataSource[0];
@@ -42,7 +37,7 @@ function getData(dataSource) {
       const countryCode = items[a][0];
       obj1.shapeid = "circle";
       obj1.label = items[a][3];
-      obj1.id = getCountryIDByCountryCode(countryCode) + markerIdSuffix;//getCountryIDByCountryCode[countryCode];//using this function, the world map is not rendering properly.
+      obj1.id = getCountryIDByCountryCode(countryCode) + markerIdSuffix;
       obj1.x = items[a][1];
       obj1.y = items[a][2];
       obj1.value = items[a][4];
