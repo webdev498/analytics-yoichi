0a69b85316aee1d775804c5c43a1710fe9f9ce3e
webdev498 <gcmobile@mail.com>
2016-06-30 16:10:56 +0530

    Update login design

--- src/layouts/NonLoggedLayout/index.js
+++ src/layouts/NonLoggedLayout/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Login from './login';
 
-import {pebbleColor} from 'theme/colors';
+import {Colors} from 'theme/colors';
 
 const styles = {
   wrap: {
     height: '100%',
     paddingTop: '50px',
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: Colors.pebble
   }
 };
 
 class NonLoggedLayout extends React.Component {
   render() {
     return (
-      <div style={styles.wrap} data-test>
+      <div style={styles.wrap}>
         <Login />
       </div>
     );
