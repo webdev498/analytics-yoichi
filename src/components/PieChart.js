fd9869f68faafb2eb3ac427803048a2e0b6aa928
webdev498 <gcmobile@mail.com>
2016-07-05 18:53:22 +0530

    working on worldMap and pieChart components

---
+++ src/components/PieChart.js
@@ -0,0 +1,282 @@
+import React, {PropTypes} from 'react';
+import FontIcon from 'material-ui/FontIcon';
+import {
+  generateRawData,
+  getIndexFromObjectName,
+  getIndexFromColumnName,
+  checkForUndefinedChartOptionObject
+} from 'utils/utils';
+
+let highlightedColor1 = '#2BD8D0', // Blue (Default colors set)
+  highlightedColor2 = '#8ABB24', // Green (Default colors set)
+  nonHighlightedColor = '#E5E5EA', // Gray (Default colors set)
+  doughnutAttributes = {},
+  countValue = 0,
+  totalValue = 0,
+  top10TotalValue = 0,
+  top10CountValue = 0,
+  background = '',
+  background2 = '',
+  color = '',
+  color2 = '',
+  transform1 = '',
+  transform2 = '',
+  calculateTransform1 = 0,
+  calculateTransform2 = 0;
+
+export function generatePieChart(inputArray) {
+  doughnutAttributes = {}; // This initialization is required
+  const doughnutInputArray1 = {
+      countValue: inputArray.countValue,
+      totalValue: inputArray.top10CountValue
+    },
+    // doughnutAttributes1 = calculateDoughnutAttributes(doughnutInputArray1, 1),
+    doughnutInputArray2 = {
+      countValue: inputArray.totalValue,
+      totalValue: inputArray.top10TotalValue
+    },
+    // doughnutAttributes2 = calculateDoughnutAttributes(doughnutInputArray2, 2),
+    percentage1 = Math.round((inputArray.top10CountValue / parseInt(inputArray.countValue)) * 100, 2),
+    percentage2 = Math.round((inputArray.top10TotalValue / parseInt(inputArray.totalValue)) * 100, 2),
+    displayPercentage1 = percentage1.toString() + '%',
+    displayPercentage2 = percentage2.toString() + '%',
+    percentage2Color = {fontWeight: 'bold', color: highlightedColor1},
+    percentage1Color = {fontWeight: 'bold', color: highlightedColor2};
+
+    console.log(displayPercentage1, displayPercentage2);
+
+  // doughnutAttributes = {
+  //   chart1Background: doughnutAttributes1.chartBackground,
+  //   chart1SliceOneStyle: doughnutAttributes1.chartSliceOneStyle,
+  //   chart1SliceTwoStyle: doughnutAttributes1.chartSliceTwoStyle,
+  //   chart2Background: doughnutAttributes2.chartBackground,
+  //   chart2SliceOneStyle: doughnutAttributes2.chartSliceOneStyle,
+  //   chart2SliceTwoStyle: doughnutAttributes2.chartSliceTwoStyle,
+  //   percentage1Color: percentage1Color,
+  //   percentage2Color: percentage2Color,
+  //   displayPercentage1: displayPercentage1,
+  //   displayPercentage2: displayPercentage2
+  // };
+  return doughnutAttributes;
+}
+
+export function generateDoughnutChart(inputArray) {
+  doughnutAttributes = {}; // This initialization is required
+  const doughnutInputArray1 = {
+      countValue: inputArray.countValue,
+      totalValue: inputArray.top10CountValue
+    },
+    doughnutAttributes1 = calculateDoughnutAttributes(doughnutInputArray1, 1),
+    doughnutInputArray2 = {
+      countValue: inputArray.totalValue,
+      totalValue: inputArray.top10TotalValue
+    },
+    doughnutAttributes2 = calculateDoughnutAttributes(doughnutInputArray2, 2),
+    percentage1 = Math.round((inputArray.top10CountValue / parseInt(inputArray.countValue)) * 100, 2),
+    percentage2 = Math.round((inputArray.top10TotalValue / parseInt(inputArray.totalValue)) * 100, 2),
+    displayPercentage1 = percentage1.toString() + '%',
+    displayPercentage2 = percentage2.toString() + '%',
+    percentage2Color = {fontWeight: 'bold', color: highlightedColor1},
+    percentage1Color = {fontWeight: 'bold', color: highlightedColor2};
+
+  doughnutAttributes = {
+    chart1Background: doughnutAttributes1.chartBackground,
+    chart1SliceOneStyle: doughnutAttributes1.chartSliceOneStyle,
+    chart1SliceTwoStyle: doughnutAttributes1.chartSliceTwoStyle,
+    chart2Background: doughnutAttributes2.chartBackground,
+    chart2SliceOneStyle: doughnutAttributes2.chartSliceOneStyle,
+    chart2SliceTwoStyle: doughnutAttributes2.chartSliceTwoStyle,
+    percentage1Color: percentage1Color,
+    percentage2Color: percentage2Color,
+    displayPercentage1: displayPercentage1,
+    displayPercentage2: displayPercentage2
+  };
+  return doughnutAttributes;
+}
+
+export function calculateDoughnutAttributes(inputArray, chartId) {
+  let {countValue, totalValue} = inputArray,
+    percentage = Math.round((totalValue / parseInt(countValue)) * 100, 2);
+
+  background = '';
+  background2 = '';
+  color = '';
+  color2 = '';
+  transform1 = '';
+  transform2 = '';
+  calculateTransform1 = 0;
+  calculateTransform2 = 0;
+
+  if (percentage === 0) {
+    percentage = Math.round((totalValue / parseInt(countValue)) * 100, 4);
+  }
+  if (percentage > 100) {
+    percentage = 100;
+  }
+  background = nonHighlightedColor;
+  color = (chartId === 1) ? highlightedColor2 : highlightedColor1;
+  background2 = background;
+  color2 = color;
+  transform1 = 'rotate(90deg)';
+  calculateTransform2 = (percentage / 100 * 360);
+  transform2 = 'rotate(' + calculateTransform2 + 'deg)';
+
+  if (percentage < 50) {
+    background = color;
+    color = background2;
+    color2 = background2;
+    calculateTransform1 = (percentage / 100 * 360 + 90);
+    transform1 = 'rotate(' + calculateTransform1 + 'deg)';
+    transform2 = 'rotate(0deg)';
+  }
+
+  const chartBackground = {background: background},
+    chartSliceOneStyle = {transform: transform1, WebkitTransform: transform1, background: color},
+    chartSliceTwoStyle = {transform: transform2, WebkitTransform: transform2, background: color2};
+
+  return {
+    chartBackground: chartBackground,
+    chartSliceOneStyle: chartSliceOneStyle,
+    chartSliceTwoStyle: chartSliceTwoStyle
+  };
+}
+
+function renderChart(props) {
+  if (!props.data) {
+    return;
+  }
+
+  const data = props.data,
+    fieldMapping = props.chartData.fieldMapping,
+    chartOptions = props.chartOptions;
+
+  let rawData = {};
+
+  highlightedColor1 = checkForUndefinedChartOptionObject(chartOptions, 'highlightedColor1', highlightedColor1);
+  highlightedColor2 = checkForUndefinedChartOptionObject(chartOptions, 'highlightedColor2', highlightedColor2);
+  nonHighlightedColor = checkForUndefinedChartOptionObject(chartOptions, 'nonHighlightedColor', nonHighlightedColor);
+
+  rawData = generateRawData(fieldMapping, data);
+
+  for (let i = 0; i < fieldMapping.length; i++) {
+    let currentChartData = fieldMapping[i],
+      {rows, columns} = rawData[currentChartData.reportId],
+      columnIndex = '';
+
+    top10CountValue = 0;
+    top10TotalValue = 0;
+
+    for (let d = 0, rowsLen = rows.length; d < rowsLen; d++) {
+      if (currentChartData.reportId === 'taf_asset_count_time_shifted') {
+        let fieldValue = '',
+          fieldName = currentChartData.columns[0],
+          fieldValueArray = [],
+          inputArray = {
+            fieldName: fieldName,
+            fieldValueArray: fieldValueArray,
+            fieldValue: fieldValue,
+            dataArray: rows[d]
+          };
+
+        countValue = getIndexFromObjectName(inputArray);
+      }
+
+      if (currentChartData.reportId === 'taf_total_usage') {
+        columnIndex = getIndexFromColumnName(currentChartData.columns, columns);
+        totalValue = rows[d][columnIndex];
+      }
+
+      if (currentChartData.reportId === 'taf_top_talkers_connections' ||
+       currentChartData.reportId === 'taf_top_talkers_bandwidth') {
+        let fieldValue = '';
+        columnIndex = getIndexFromColumnName(currentChartData.columns, columns);
+        fieldValue = rows[d][columnIndex];
+        let value = Math.round(((fieldValue * 100) / totalValue), 2);
+        if (value > 0) {
+          top10CountValue = top10CountValue + 1;
+          top10TotalValue = top10TotalValue + parseInt(fieldValue);
+        }
+      }
+    }
+  }
+
+  let inputArray = {
+    countValue: countValue,
+    top10CountValue: top10CountValue,
+    top10TotalValue: top10TotalValue,
+    totalValue: totalValue.toPrecision()
+  };
+
+  doughnutAttributes = generateDoughnutChart(inputArray);
+  // generatePieChart(inputArray);
+}
+
+// function loadAssetIcons() {
+//   let icon;
+//   for (let a = 0; a < 10; a++) {
+//     icon += loadAssetIcon();
+//   }
+//   return icon;
+// }
+
+function loadAssetIcon() {
+  return (
+    <FontIcon className='material-icons'
+      style={{color: '#2BD8D0'}}>
+      desktop_mac
+    </FontIcon>
+  );
+}
+
+class DoughnutChart extends React.Component {
+  static propTypes = {
+    attributes: PropTypes.object,
+    tableOptions: PropTypes.object
+  }
+  render() {
+    const {props} = this;
+    return (
+      <div style={props.attributes.chartBorder}>{renderChart(props)}
+        <div style={props.attributes.chartCaption}>{props.meta.title}</div>
+        <div style={{width: '100%', display: 'flex', flexWrap: 'wrap'}}>
+          <div className='pieCard'>
+            { /* <div className='percentage1 chart'>{doughnutAttributes.displayPercentage2}</div> */ }
+            <div className='pie-chart chart' style={doughnutAttributes.chart2Background}>
+              <div className='slice one' style={doughnutAttributes.chart2SliceOneStyle}></div>
+              <div className='slice two' style={doughnutAttributes.chart2SliceTwoStyle}></div>
+              <div className='chart-center'><span>{doughnutAttributes.displayPercentage2}</span></div>
+            </div>
+          </div>
+          { /* <div className='card2'>
+            <div className='donut-chart chart1' style={doughnutAttributes.chart1Background}>
+              <div className='slice one' style={doughnutAttributes.chart1SliceOneStyle}></div>
+              <div className='slice two' style={doughnutAttributes.chart1SliceTwoStyle}></div>
+              <div className='chart-center'><span>{doughnutAttributes.displayPercentage1}</span></div>
+            </div>
+          </div> */ }
+          <div style={{marginTop: '30px', marginRight: '60px'}}>
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}<br />
+            {loadAssetIcon()}
+          </div>
+          <div id='connectionsPercentage'><br /><br />
+            <span style={{fontSize: '35px', color: '#2BD8D0'}}>{doughnutAttributes.displayPercentage2}</span>
+            <br /><span style={{fontSize: '13px', color: '#6b7282'}}>{props.meta.legend[0]}</span>
+            <br /><span style={{fontSize: '13px', color: '#cbcbd1'}}>{props.meta.legend[1]}</span>
+            <br /><span style={{fontSize: '35px', color: '#2BD8D0'}}>{doughnutAttributes.displayPercentage1}</span>
+            <br /><span style={{fontSize: '13px', color: '#6b7282'}}>{props.meta.legend[2]}</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DoughnutChart;
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -13,7 +13,9 @@ import {
 function generateChartDataSource(rawData, props) {
   const {chartOptions, chartData, shapes} = props;
   let markersItemsObject = [],
-    markerIdSuffix = 0;
+    markerIdSuffix = 0,
+    dataObject = [],
+    connectionsValues = [];
 
   for (let i = 0; i < chartData.fieldMapping.length; i++) {
     let currentChartData = chartData.fieldMapping[i],
@@ -32,6 +34,7 @@ function generateChartDataSource(rawData, props) {
 
     // Get column data for x-axis
     // if (columnIndexArray.length !== 0) {
+    let valueIndex = 0;
     for (let a = 0, rowsLen = rows.length; a < rowsLen; a++) {
       let obj1 = {};
       if (rows[a][1] === 'N/A' || rows[a][2] === 'N/A') {
@@ -39,16 +42,27 @@ function generateChartDataSource(rawData, props) {
       }
       else {
         let countryCode = rows[a][0];
-        obj1.shapeid = currentChartData.shapeid;
-        obj1.label = rows[a][3];
-        obj1.id = getCountryIDByCountryCode(countryCode) + markerIdSuffix;
-        obj1.x = rows[a][1];
-        obj1.y = rows[a][2];
-        obj1.value = rows[a][4];
-        obj1.alpha = currentChartData.alpha;
+        if (currentChartData.shapeid !== 'circle') {
+          obj1.shapeid = currentChartData.shapeid;
+          obj1.label = rows[a][3];
+          obj1.id = getCountryIDByCountryCode(countryCode) + markerIdSuffix;
+          obj1.x = rows[a][1];
+          obj1.y = rows[a][2];
+          obj1.value = rows[a][4];
+          obj1.alpha = currentChartData.alpha;
 
-        markersItemsObject.push(obj1);
-        markerIdSuffix = markerIdSuffix + 1;
+          markersItemsObject.push(obj1);
+          markerIdSuffix = markerIdSuffix + 1;
+        }
+        if (currentChartData.shapeid === 'circle') {
+          obj1.id = getCountryIDByCountryCode(countryCode);
+          obj1.value = rows[a][4].toString();
+          if (rows[a][4] !== null) {
+            connectionsValues[valueIndex] = rows[a][4];
+            valueIndex++;
+          }
+          dataObject.push(obj1);
+        }
       }
     }
   }
@@ -63,7 +77,7 @@ function generateChartDataSource(rawData, props) {
     'showMarkerLabels': '0',
     'showvalue': '0',
     'autoScaleMarkers': '0',
-    'showLegend': '0',
+    'showLegend': '1',
     'chartLeftMargin': '0',
     'chartRightMargin': '0',
     'chartTopMargin': '0',
@@ -80,10 +94,65 @@ function generateChartDataSource(rawData, props) {
     shapesObject = shapes;
   }
 
-  dataSourceObject.markers = {
-    'shapes': shapesObject,
-    'items': markersItemsObject
+  // dataSourceObject.markers = {
+  //   'shapes': shapesObject,
+  //   'items': markersItemsObject
+  // };
+
+  // let colorRanges = [];
+  // for ()
+
+  dataSourceObject.colorrange = {
+    'minvalue': '0',
+    'startlabel': 'Less',
+    'endlabel': 'More',
+    'code': 'BAF2F0',
+    'gradient': '1',
+    'color': [
+      // {
+      //   'maxvalue': 50,
+      //   'code': 'f8bd19'
+      // },
+      {
+        'maxvalue': Math.max.apply(Math, connectionsValues),
+        'code': '2BD8D0'
+      }
+    ],
+    'maxvalue': 0
   };
+
+   // "colorrange": {
+   //      "color": [
+   //          {
+   //              "minvalue": "0",
+   //              "maxvalue": "100",
+   //              "code": "#D0DFA3",
+   //              "displayValue": "< 100M"
+   //          },
+   //          {
+   //              "minvalue": "100",
+   //              "maxvalue": "500",
+   //              "code": "#B0BF92",
+   //              "displayValue": "100-500M"
+   //          },
+   //          {
+   //              "minvalue": "500",
+   //              "maxvalue": "1000",
+   //              "code": "#91AF64",
+   //              "displayValue": "500M-1B"
+   //          },
+   //          {
+   //              "minvalue": "1000",
+   //              "maxvalue": "5000",
+   //              "code": "#A9FF8D",
+   //              "displayValue": "> 1B"
+   //          }
+   //      ]
+   //  },
+
+  dataSourceObject.data = [{data: dataObject}];
+
+  // console.log(JSON.stringify(dataSourceObject));
   return dataSourceObject;
 }
 
@@ -138,12 +207,236 @@ class WorldMap extends React.Component {
       });
       fusioncharts.render();
     });
+
+    /*FusionCharts.ready(function() {
+      var worldMap = new FusionCharts({
+        type: 'maps/worldwithcountries',
+        renderAt: 'chart-container',
+        width: '600',
+        height: '400',
+        dataFormat: 'json',
+        dataSource: {
+          'chart': {
+            'entityFillHoverColor': '#cccccc',
+            'nullEntityColor': 'aaaaaa',
+            'showLabels': '0',
+            'theme': 'zune',
+            'useValuesForMarkers': '1',
+            'showMarkerLabels': '0',
+            'showvalue': '0',
+            'autoScaleMarkers': '0',
+            'showLegend': '0',
+            'chartLeftMargin': '0',
+            'chartRightMargin': '0',
+            'chartTopMargin': '0',
+            'chartBottomMargin': '0',
+            'markerBgColor': '#00AFF0',
+            'alignCaptionWithCanvas': '0',
+            'captionFontSize': '14',
+            'captionFontColor': '#555555',
+            'bgAlpha': '0'
+          },
+          'markers': {
+            'shapes': [
+              {
+                'labelPadding': '15',
+                'xscale': '30',
+                'id': 'maliciousIcon',
+                'type': 'image',
+                'yscale': '30',
+                'url': '/img/biohazard.png'
+              }
+            ],
+            'items': [
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'China',
+                'id': '10124',
+                'x': '1681.14',
+                'y': '479.81',
+                'value': 50,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'Netherlands',
+                'id': '15725',
+                'x': '1034.59',
+                'y': '381.81',
+                'value': 20,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'United States',
+                'id': '2326',
+                'x': '547.14',
+                'y': '484.9',
+                'value': 16,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'South Korea',
+                'id': '11027',
+                'x': '1756.23',
+                'y': '497.63',
+                'value': 10,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'Mexico',
+                'id': '1628',
+                'x': '441.5',
+                'y': '618.54',
+                'value': 5,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'Bangladesh',
+                'id': '9629',
+                'x': '1533.5',
+                'y': '581.63',
+                'value': 3,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'Japan',
+                'id': '10730',
+                'x': '1827.5',
+                'y': '504',
+                'value': 2,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'Switzerland',
+                'id': '16831',
+                'x': '1049.87',
+                'y': '426.36',
+                'value': 1,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'Sweden',
+                'id': '16732',
+                'x': '1105.87',
+                'y': '319.45',
+                'value': 1,
+                'alpha': '100'
+              },
+              {
+                'shapeid': 'maliciousIcon',
+                'label': 'Vietnam',
+                'id': '12533',
+                'x': '1623.87',
+                'y': '594.36',
+                'value': 1,
+                'alpha': '100'
+              }
+            ]
+          },
+          'colorrange': {
+            'minvalue': '0',
+            'startlabel': 'Low',
+            'endlabel': 'High',
+            'code': 'e44a00',
+            'gradient': '1',
+            'color': [
+              {
+                'maxvalue': 50,
+                'code': 'f8bd19'
+              },
+              {
+                'maxvalue': 1000,
+                'code': '6baa01'
+              }
+            ],
+            'maxvalue': 0
+          },
+          'data': [
+            {
+              'data': [
+                {
+                  'id': '104',
+                  'value': '1000'
+                }
+              ]
+            }
+          ]
+        }
+      }).render();
+    });*/
+    /*FusionCharts.ready(function() {
+      var airportsMap = new FusionCharts({
+        type: 'maps/worldwithcountries',
+        renderAt: 'chart-container',
+        width: '600',
+        height: '400',
+        dataFormat: 'json',
+        dataSource: {
+          'chart': {
+            'theme': 'zune',
+            'showMarkerLabels': '1',
+            'entityFillColor': '#A8A8A8',
+            'entityFillHoverColor': '#E5E5E9'
+          },
+          'markers': {
+            'shapes': [{
+              'id': 'myCustomShape',
+              'type': 'image',
+              'url': 'http://static.fusioncharts.com/docs/assets/airplane-99047_150.png',
+              'xscale': '35',
+              'yscale': '35',
+
+              'labelPadding': '15'
+            }],
+            'items': [
+              {
+                'id': 'lon',
+                'shapeid': 'myCustomShape',
+                'x': '340.23',
+                'y': '125.9',
+                'label': 'LHR',
+                'tooltext': 'Heathrow International Airport {br}IACL Code : EGLL',
+                'labelpos': 'left'
+              }
+            ]
+          },
+          'colorrange': {
+            'minvalue': '0',
+            'startlabel': 'Low',
+            'endlabel': 'High',
+            'code': 'e44a00',
+            'gradient': '1',
+            'color': [{
+              'maxvalue': 50,
+              'code': 'f8bd19'
+            }, {
+              'maxvalue': 1000,
+              'code': '6baa01'
+            }],
+            'maxvalue': 0
+          },
+          'data': [{
+            'data': [{
+              'id': '104',
+              'value': '1000'
+            }]
+          }]
+        }
+      }).render();
+    });*/
   }
 
   render() {
     const {props} = this;
     return (
-      <div style={{width: '100%'}}>
+      <div style={{width: '70%'}}>
         <div className='chartCaption'>{props.meta.subTitle}</div>
         <div style={{textAlign: 'center'}}><br />
           <img src='img/biohazard.png' width='20' height='20' />&nbsp;Malicious Connections
