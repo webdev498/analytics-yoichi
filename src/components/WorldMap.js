0c9b315ed1548c53a83be0a459a8963f6236d093
webdev498 <gcmobile@mail.com>
2016-07-06 15:19:03 +0530

    Displayed new legends for worldmaps and worked on alignments of worldmap

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -85,7 +85,7 @@ function generateChartDataSource(rawData, props) {
     'showMarkerLabels': '0',
     'showvalue': '0',
     'autoScaleMarkers': '0',
-    'showLegend': '1',
+    'showLegend': '0',
     'chartLeftMargin': '0',
     'chartRightMargin': '0',
     'chartTopMargin': '0',
@@ -114,7 +114,7 @@ function generateChartDataSource(rawData, props) {
     'minvalue': '0',
     'startlabel': 'Less',
     'endlabel': 'More',
-    'code': 'BAF2F0',
+    'code': 'DBF8F7',
     'gradient': '1',
     'color': [
       // {
@@ -205,7 +205,7 @@ class WorldMap extends React.Component {
         containerBackgroundOpacity: '0',
         dataSource: generateChartDataSource(rawData, props),
         events: {
-          markerClick: function(eventObj, dataObj) {
+          entityClick: function(eventObj, dataObj) {
             const url = getMarkerClickUrl(props, dataObj);
             if (url !== '' && !isUndefined(url)) {
               clickThrough(url);
@@ -215,242 +215,41 @@ class WorldMap extends React.Component {
       });
       fusioncharts.render();
     });
-
-    /*FusionCharts.ready(function() {
-      var worldMap = new FusionCharts({
-        type: 'maps/worldwithcountries',
-        renderAt: 'chart-container',
-        width: '600',
-        height: '400',
-        dataFormat: 'json',
-        dataSource: {
-          'chart': {
-            'entityFillHoverColor': '#cccccc',
-            'nullEntityColor': 'aaaaaa',
-            'showLabels': '0',
-            'theme': 'zune',
-            'useValuesForMarkers': '1',
-            'showMarkerLabels': '0',
-            'showvalue': '0',
-            'autoScaleMarkers': '0',
-            'showLegend': '0',
-            'chartLeftMargin': '0',
-            'chartRightMargin': '0',
-            'chartTopMargin': '0',
-            'chartBottomMargin': '0',
-            'markerBgColor': '#00AFF0',
-            'alignCaptionWithCanvas': '0',
-            'captionFontSize': '14',
-            'captionFontColor': '#555555',
-            'bgAlpha': '0'
-          },
-          'markers': {
-            'shapes': [
-              {
-                'labelPadding': '15',
-                'xscale': '30',
-                'id': 'maliciousIcon',
-                'type': 'image',
-                'yscale': '30',
-                'url': '/img/biohazard.png'
-              }
-            ],
-            'items': [
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'China',
-                'id': '10124',
-                'x': '1681.14',
-                'y': '479.81',
-                'value': 50,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'Netherlands',
-                'id': '15725',
-                'x': '1034.59',
-                'y': '381.81',
-                'value': 20,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'United States',
-                'id': '2326',
-                'x': '547.14',
-                'y': '484.9',
-                'value': 16,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'South Korea',
-                'id': '11027',
-                'x': '1756.23',
-                'y': '497.63',
-                'value': 10,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'Mexico',
-                'id': '1628',
-                'x': '441.5',
-                'y': '618.54',
-                'value': 5,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'Bangladesh',
-                'id': '9629',
-                'x': '1533.5',
-                'y': '581.63',
-                'value': 3,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'Japan',
-                'id': '10730',
-                'x': '1827.5',
-                'y': '504',
-                'value': 2,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'Switzerland',
-                'id': '16831',
-                'x': '1049.87',
-                'y': '426.36',
-                'value': 1,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'Sweden',
-                'id': '16732',
-                'x': '1105.87',
-                'y': '319.45',
-                'value': 1,
-                'alpha': '100'
-              },
-              {
-                'shapeid': 'maliciousIcon',
-                'label': 'Vietnam',
-                'id': '12533',
-                'x': '1623.87',
-                'y': '594.36',
-                'value': 1,
-                'alpha': '100'
-              }
-            ]
-          },
-          'colorrange': {
-            'minvalue': '0',
-            'startlabel': 'Low',
-            'endlabel': 'High',
-            'code': 'e44a00',
-            'gradient': '1',
-            'color': [
-              {
-                'maxvalue': 50,
-                'code': 'f8bd19'
-              },
-              {
-                'maxvalue': 1000,
-                'code': '6baa01'
-              }
-            ],
-            'maxvalue': 0
-          },
-          'data': [
-            {
-              'data': [
-                {
-                  'id': '104',
-                  'value': '1000'
-                }
-              ]
-            }
-          ]
-        }
-      }).render();
-    });*/
-    /*FusionCharts.ready(function() {
-      var airportsMap = new FusionCharts({
-        type: 'maps/worldwithcountries',
-        renderAt: 'chart-container',
-        width: '600',
-        height: '400',
-        dataFormat: 'json',
-        dataSource: {
-          'chart': {
-            'theme': 'zune',
-            'showMarkerLabels': '1',
-            'entityFillColor': '#A8A8A8',
-            'entityFillHoverColor': '#E5E5E9'
-          },
-          'markers': {
-            'shapes': [{
-              'id': 'myCustomShape',
-              'type': 'image',
-              'url': 'http://static.fusioncharts.com/docs/assets/airplane-99047_150.png',
-              'xscale': '35',
-              'yscale': '35',
-
-              'labelPadding': '15'
-            }],
-            'items': [
-              {
-                'id': 'lon',
-                'shapeid': 'myCustomShape',
-                'x': '340.23',
-                'y': '125.9',
-                'label': 'LHR',
-                'tooltext': 'Heathrow International Airport {br}IACL Code : EGLL',
-                'labelpos': 'left'
-              }
-            ]
-          },
-          'colorrange': {
-            'minvalue': '0',
-            'startlabel': 'Low',
-            'endlabel': 'High',
-            'code': 'e44a00',
-            'gradient': '1',
-            'color': [{
-              'maxvalue': 50,
-              'code': 'f8bd19'
-            }, {
-              'maxvalue': 1000,
-              'code': '6baa01'
-            }],
-            'maxvalue': 0
-          },
-          'data': [{
-            'data': [{
-              'id': '104',
-              'value': '1000'
-            }]
-          }]
-        }
-      }).render();
-    });*/
   }
 
   render() {
     const {props} = this;
     return (
       <div style={{width: '70%'}}>
-        <div className='chartCaption'>{props.meta.subTitle}</div>
-        <div style={{textAlign: 'center'}}><br />
-          <img src='img/biohazard.png' width='20' height='20' />&nbsp;Malicious Connections
+        <div style={{fontSize: '14px', color: '#6b7282', fontWeight: '600', position: 'absolute'}}>
+          {props.meta.subTitle}
         </div>
-        <div id={props.attributes.id}></div>
+        <div id={props.attributes.id} style={{marginTop: '-40px'}}></div>
         {this.renderChart(props)}
+        <div style={{display: 'flex', flexWrap: 'wrap', marginTop: '-40px'}}>
+          <div>
+            <span style={{fontSize: '11px', color: '#6b7282'}}>Secure Connections</span><br />
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+              <div style={{backgroundColor: '#2BD8D0', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#51DFD8', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#71E5DF', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#97ECE8', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#BAF2F0', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#DBF8F7', width: '20px', height: '12px'}}></div>
+            </div>
+          </div>
+          <div style={{marginLeft: '40px'}}>
+            <span style={{fontSize: '11px', color: '#6b7282'}}>Malicious Connections</span><br />
+            <div style={{display: 'flex', flexWrap: 'wrap'}}>
+              <div style={{backgroundColor: '#F69275', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#F7A48B', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#F9B6A2', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#F8CABB', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#FCDBD2', width: '20px', height: '12px'}}></div>
+              <div style={{backgroundColor: '#FEEDE8', width: '20px', height: '12px'}}></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
