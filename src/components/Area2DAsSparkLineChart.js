01c0cc0c91052681275f26a6c1d213f0a92b1cce
webdev498 <gcmobile@mail.com>
2016-07-20 12:27:09 +0530

    modified unit tests of doughnutchart component

--- src/components/Area2DAsSparkLineChart.js
+++ src/components/Area2DAsSparkLineChart.js
@@ -2,6 +2,8 @@ import React, {PropTypes} from 'react';
 import {Colors} from 'theme/colors';
 
 function generateChartDataSource(chartProperties, duration) {
+  console.log(JSON.stringify(chartProperties));
+  console.log(JSON.stringify(duration));
   let dataSourceObject = {},
     dataSet = [],
     dataObject = [],
@@ -49,6 +51,7 @@ function generateChartDataSource(chartProperties, duration) {
   if (dataSet.length > 0) {
     dataSourceObject.dataset = dataSet;
   }
+  console.log(JSON.stringify(dataSourceObject));
   return dataSourceObject;
 }
 
---
+++ tests/components/area2DAsSparkLineChart.spec.js
@@ -0,0 +1,32 @@
+import {
+  generateDoughnutChart,
+  calculateDoughnutAttributes
+} from 'components/DoughnutChart';
+
+describe('DoughnutChart Component: ', function() {
+  it('generateDoughnutChart should return doughnut chart attributes.', function() {
+    const inputArray = {'countValue': 6, 'top10CountValue': 4, 'top10TotalValue': 21581, 'totalValue': '21722'},
+      doughnutChartAttributes = {'chart1Background': {'background': '#CCCCCC'},
+      'chart1SliceOneStyle': {'transform': 'rotate(90deg)', 'WebkitTransform': 'rotate(90deg)',
+      'background': '#8ABB24'}, 'chart1SliceTwoStyle': {'transform': 'rotate(241.20000000000002deg)',
+      'WebkitTransform': 'rotate(241.20000000000002deg)', 'background': '#8ABB24'},
+      'chart2Background': {'background': '#CCCCCC'},
+      'chart2SliceOneStyle': {'transform': 'rotate(90deg)', 'WebkitTransform': 'rotate(90deg)',
+      'background': '#5E2B78'}, 'chart2SliceTwoStyle': {'transform': 'rotate(356.4deg)',
+      'WebkitTransform': 'rotate(356.4deg)', 'background': '#5E2B78'},
+      'percentage1Color': {'fontWeight': 'bold', 'color': '#8ABB24'},
+      'percentage2Color': {'fontWeight': 'bold', 'color': '#5E2B78'}, 'displayPercentage1': '67%',
+      'displayPercentage2': '99%'};
+    expect(generateDoughnutChart(inputArray)).to.deep.equal(doughnutChartAttributes);
+  });
+
+  it('calculateDoughnutAttributes should compute doughnut chart attributes.', function() {
+    const inputArray = {'countValue': 6, 'totalValue': 4},
+      chartId = 1,
+      doughnutChartAttributes = {'chartBackground': {'background': '#CCCCCC'},
+      'chartSliceOneStyle': {'transform': 'rotate(90deg)', 'WebkitTransform': 'rotate(90deg)',
+      'background': '#8ABB24'}, 'chartSliceTwoStyle': {'transform': 'rotate(241.20000000000002deg)',
+      'WebkitTransform': 'rotate(241.20000000000002deg)', 'background': '#8ABB24'}};
+    expect(calculateDoughnutAttributes(inputArray, chartId)).to.deep.equal(doughnutChartAttributes);
+  });
+});
