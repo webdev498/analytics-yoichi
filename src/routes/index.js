24d79300b800f19c47bd14afc31934ac4def2835
webdev498 <gcmobile@mail.com>
2016-09-02 21:19:27 +0530

    Added route for new alert page

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,6 +26,7 @@ export default (store) => {
         <IndexRoute />
         <Route path='alerts' />
         <Route path='alert/:alertId/:date' />
+        <Route path='alert-new/:alertId/:date' />
         <Route path='country' />
         <Route path='traffic' />
         <Route path='assets' />
