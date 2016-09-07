cc9e5708f90f2e41f9a3c4d29274841850f9928f
webdev498 <gcmobile@mail.com>
2016-09-07 15:28:19 +0530

    Update config file

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ window.global = window.global || {};
 const global = window.global;
 
 export const baseUrl = (global.baseUrl !== undefined) ? global.baseUrl : 'https://demo.ranksoftwareinc.com';
-export const loginUrl = baseUrl + '/oauth/authorize';
+export const loginUrl = global.loginUrl || (baseUrl + '/oauth/authorize');
 export const responseType = 'token';
 export const clientId = 'taf_dashboard';
 export const redirectUri = global.redirectUri || 'http://localhost:3000/#/dashboard?';
