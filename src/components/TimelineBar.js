5fbd5f33b0ea3a12badfb38aa2a29dd2dff54765
webdev498 <gcmobile@mail.com>
2016-09-26 17:22:42 +0530

    removed commented code

--- src/components/TimelineBar.js
+++ src/components/TimelineBar.js
@@ -20,6 +20,14 @@ class TimelineGraph extends React.Component {
     super(props);
     this.state = {
       'selectedAreaStyle': {
+        'marginTop': '0px', // ((baseHeight * (100 - (sliderValue + sliderRange)) / 100) - 12.8) + 'px',
+        'height': '65px', // (baseHeight * ((sliderValue + sliderRange) - (sliderValue - sliderRange)) / 100) + 'px',
+        'width': timelineBarWidth + 'px',
+        'position': 'absolute',
+        'marginLeft': '5px',
+        'background': Colors.smoke,
+        'zIndex': 1000,
+        'opacity': 0.7
       }
     };
 
