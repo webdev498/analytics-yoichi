e7b721ba37f6abf5aafff8a84c20ececc0d50330
webdev498 <gcmobile@mail.com>
2016-09-29 18:10:11 +0530

    Updated timeline component to remove unused code

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,50 +1,13 @@
 import React, {PropTypes} from 'react';
 import {Colors} from 'theme/colors';
 import PaginationWidget from 'components/PaginationWidget';
-// import TimelineBar from '../components/TimelineBar';
 import TrafficEvents from '../components/TrafficEvents';
 import Alerts from '../components/Alerts';
 import Loader from '../components/Loader';
 import {formatDateInLocalTimeZone} from 'utils/utils';
+import {fetchData} from 'utils/timelineUtils';
 
-import {
-  fetchData,
-  getPosition
-} from 'utils/timelineUtils';
-
-let timeWindow = '1h',
-  sliderRange = 4,
-  topMarginLag = 980;
-
-function getDateOfSelectedEvents(barId, dateString, selectedMin, selectedMax, displayCount) {
-  let topPositions = getPosition(document.getElementById(barId));
-  if (topPositions.y !== undefined) {
-    let top = topPositions.y - topMarginLag;
-    // console.log(barId, selectedMin, selectedMax, topPositions.y, top);
-    if (selectedMin !== '' && selectedMax !== '') {
-      if (selectedMin <= top && selectedMax >= top) {
-        if (document.getElementById(barId) !== undefined && document.getElementById(barId) !== null &&
-          displayCount < sliderRange) {
-          document.getElementById(barId).style.backgroundColor = Colors.timelineBarColors[0];
-          displayCount++;
-        }
-      }
-      else {
-        if (document.getElementById(barId) !== undefined && document.getElementById(barId) !== null) {
-          document.getElementById(barId).style.backgroundColor = Colors.timelineBarColors[1];
-        }
-        dateString = '';
-      }
-    }
-    else {
-      if (document.getElementById(barId) !== undefined && document.getElementById(barId) !== null) {
-        document.getElementById(barId).style.backgroundColor = Colors.timelineBarColors[1];
-      }
-      dateString = '';
-    }
-  }
-  return dateString;
-}
+let timeWindow = '1h';
 
 class Timeline extends React.Component {
   static propTypes = {
@@ -77,7 +40,6 @@ class Timeline extends React.Component {
       'selectedMax': 55,
       'timelineType': attributes.type,
       'alertDate': params.date,
-      // 'filter': props.meta.api.queryParams.filter,
       'totalCount': 0,
       'totalPage': 0,
       'currentPage': 1,
@@ -98,7 +60,6 @@ class Timeline extends React.Component {
     this.displayData = this.displayData.bind(this);
     this.getRows = this.getRows.bind(this);
     this.timelineBarLoaded = this.timelineBarLoaded.bind(this);
-    // this.setTopMarginLag = this.setTopMarginLag.bind(this);
   }
 
   setSelectedSliderValues(selectedMin, selectedMax) {
@@ -144,49 +105,28 @@ class Timeline extends React.Component {
         );
       }
     }
-    topMarginLag = props.attributes.topMarginLag;
   }
 
   displayEvents(selectedMin, selectedMax) {
-    // if (!this.state.timelineBarLoaded) {
-    //   return;
-    // }
     const rows = this.state.rows,
-      // displaySelectedRows = this.state.displaySelectedRows,
       that = this;
 
-    let displayCount = 0,
-      mainDivStyle = {
-        width: '675px',
-        // overflow: 'hidden'
-      };
-      // scrollbarStyle = displaySelectedRows ? '' : 'scrollbarStyle';
-
-    // if (!displaySelectedRows) {
-    //   mainDivStyle = Object.assign(mainDivStyle, {
-    //     overflowX: 'hidden',
-    //     overflowY: 'auto'
-    //   });
-    // } className={scrollbarStyle}
+    let mainDivStyle = {
+      width: '675px'
+    };
 
     return (
       <div style={mainDivStyle}>
         {
           rows.map(function(event, index) {
             let dateString = event[0].date,
-              newLine = '<br />',
               dateTime = {
                 date: '',
                 time: ''
               },
               barId = 'bar' + index;
 
-            // if (displaySelectedRows) {
-            //   dateString = getDateOfSelectedEvents(barId, dateString, selectedMin, selectedMax, displayCount);
-            // }
-
             if (dateString !== '') {
-              newLine = (index === 0) ? '' : '<br />';
               dateTime = formatDateInLocalTimeZone(dateString);
               return (
                 <div style={{display: 'flex'}}>
@@ -206,101 +146,51 @@ class Timeline extends React.Component {
   }
 
   fetchData(that, pageNumber, type) {
-    // return (event) => {
+    that.setState({
+      'isFetching': true
+    });
+
+    let parameters = {
+      pageNumber: (pageNumber - 1) * that.state.pageSize,
+      timelineType: that.state.timelineType,
+      duration: timeWindow,
+      alertDate: that.state.alertDate,
+      filter: that.props.data.options.customParams.filter,
+      pageSize: that.state.pageSize
+    };
+
+    const fetchedData = fetchData(parameters, type);
+
+    if (!fetchedData) {
       that.setState({
-        'isFetching': true
+        isFetching: false
       });
-
-      let parameters = {};
-
-      // if (type === 'traffic') {
-        parameters = {
-          pageNumber: (pageNumber - 1) * that.state.pageSize,
-          timelineType: that.state.timelineType,
-          duration: timeWindow,
-          alertDate: that.state.alertDate,
-          filter: that.props.data.options.customParams.filter,
-          pageSize: that.state.pageSize
-        };
-      // }
-
-      // if (type === 'alert') {
-      //   parameters = {
-      //     duration: timeWindow,
-      //     reportId: 'taf_alert_by_asset' // kept this hardcoded for now.
-      //     // Later, I will have to use fetchApiData function from props object
-      //   };
-      // }
-
-      const fetchedData = fetchData(parameters, type);
-
-      if (!fetchedData) {
+      return;
+    }
+    fetchedData.then(
+      function(json) {
         that.setState({
-          isFetching: false
+          'isFetching': false,
+          'currentPage': pageNumber,
+          'rows': json.rows,
+          'nextPageStart': json.next,
+          'selectedMin': 0,
+          'selectedMax': 55
         });
-        return;
       }
-      fetchedData.then(
-        function(json) {
-          console.log('pageNumber', pageNumber);
-          that.setState({
-            'isFetching': false,
-            'currentPage': pageNumber,
-            'rows': json.rows,
-            // 'nextPageStart': (pageNumber > 1) ? (that.state.nextPageStart + that.state.pageSize)
-            //   : that.state.nextPageStart,
-            'nextPageStart': json.next,
-            'selectedMin': 0,
-            'selectedMax': 55
-          });
-        }
-      );
-    // };
+    );
   }
 
   pageChanged() {
     const that = this;
-    return (pageNumber, e) => {
-      console.log('pageNumber', pageNumber);
-      e.preventDefault();
-      // let api = {};
-      // if (that.state.type === 'traffic') {
-        // api = {
-        //   'path': '/api/alert/traffic',
-        //   'queryParams': {
-        //     'window': timeWindow,
-        //     'count': this.state.pageSize,
-        //     'from': (pageNumber === 1) ? (this.state.nextPageStart + this.state.pageSize) : this.state.nextPageStart,
-        //     // 'filter': this.props.data.options.customParams.filter !== undefined
-        //     //   ? this.props.data.options.customParams.filter
-        //     //   : '',
-        //     'filter': '((source.ip = 10.3.162.105 AND (type = conn AND destination.port > 1024 AND destination.internal != true)) OR (source.ip = 10.3.162.105 AND (type = http AND data.http.host % "www.download.windowsupdate.com" AND data.http.userAgent % "Microsoft-CryptoAPI")))',
-        //     'date': this.state.alertDate
-        //   },
-        //   'pathParams': {}
-        // };
-      // }
-
-      // if (that.state.type === 'alert') {
-      //   api = {
-      //     'path': '/api/analytics/reporting/execute/{reportId}',
-      //     'queryParams': {
-      //       'window': timeWindow
-      //     },
-      //     'pathParams': {
-      //       'reportId': 'taf_alert_by_asset'
-      //     }
-      //   };
-      // }
-      // that.props.fetchApiData(that.props.id, api, that.props.params);
+    return (pageNumber) => {
       this.fetchData(that, pageNumber, that.state.type);
     };
   }
 
   prevPageChanged() {
     const that = this;
-    return (pageNumber, e) => {
-      e.preventDefault();
+    return (pageNumber) => {
       if ((parseInt(pageNumber) - 1) > 0) {
         this.fetchData(that, pageNumber - 1, that.state.type);
       }
@@ -309,8 +199,7 @@ class Timeline extends React.Component {
 
   nextPageChanged() {
     const that = this;
-    return (pageNumber, pageSize, e) => {
-      e.preventDefault();
+    return (pageNumber, pageSize) => {
       if ((parseInt(pageNumber) + 1) <= pageSize) {
         this.fetchData(that, pageNumber + 1, that.state.type);
       }
@@ -323,14 +212,6 @@ class Timeline extends React.Component {
     }
   }
 
-  // setTopMarginLag() {
-  //   const {props} = this;
-  //   if (document.getElementById(props.id) !== null) {
-  //     let offsetTop = document.getElementById(props.id).offsetTop;
-  //     console.log('offsetTop', offsetTop, props.id);
-  //   }
-  // }
-
   render() {
     return (
       <div>{this.getRows()}
@@ -338,20 +219,10 @@ class Timeline extends React.Component {
         {
           (this.state.rows.length > 0)
             ? <div>
-              {/*<TimelineBar id={this.state.id}
-                data={this.state.rows}
-                setSelectedSliderValues={this.setSelectedSliderValues}
-                duration={timeWindow}
-                timelineBarLoaded={this.timelineBarLoaded}
-                isLoaded={this.state.timelineBarLoaded}
-                selectedAreaStyle={this.state.selectedAreaStyle} />*/}
-
               <div>
-                { /*  style={{'marginLeft': '150px', 'position': 'absolute'}}
-                // This is needed after we display Slider navigation*/ }
                 {this.displayEvents(this.state.selectedMin, this.state.selectedMax)}
               </div>
-              <div style={{padding: '15px'}}></div>
+              <div style={{padding: '15px'}} />
 
               <PaginationWidget Size={this.state.totalPage}
                 onPageChanged={this.pageChanged()}
@@ -362,7 +233,6 @@ class Timeline extends React.Component {
             </div>
           : null
         }
-        {/*{this.setTopMarginLag()}*/}
       </div>
     );
   }
