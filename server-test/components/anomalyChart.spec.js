f53a30a39f68e221ca8d124840dfa0587531bf2d
webdev498 <gcmobile@mail.com>
2016-10-28 20:30:29 +0530

    Setup test for the node files

---
+++ server-test/components/anomalyChart.spec.js
@@ -0,0 +1,6 @@
+import anomalyChart from '../../server/components/anomalyChart';
+
+// DAL stands for Data Abstraction Layer.
+describe('Anomaly Chart DAL', () => {
+  console.log(anomalyChart);
+})
