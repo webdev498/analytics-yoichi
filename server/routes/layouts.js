99060c9f9bfde6337e97b99489a64c29e4071a9c
webdev498 <gcmobile@mail.com>
2016-11-11 19:07:40 +0530

    Layout logic in middle

--- server/routes/layouts.js
+++ server/routes/layouts.js
@@ -1,3 +1,7 @@
+import https from 'https';
+import fetch from 'node-fetch';
+import {layoutBaseUrl} from '../../serverEnv';
+
 const fs = require('fs');
 const path = require('path');
 
@@ -10,6 +14,8 @@ const readFileThunk = function(src) {
   });
 };
 
+const agent = new https.Agent({ rejectUnauthorized: false });
+
 export default async function layoutRoutes(ctx, next) {
   const reqPath = ctx.request.path;
   const layout = reqPath.split('/')[4];
@@ -18,9 +24,34 @@ export default async function layoutRoutes(ctx, next) {
     filePath = path.join(__dirname, fileName);
 
   console.log('filePath', filePath);
-  const body = await readFileThunk(filePath);
 
-  ctx.set('Content-Type', 'application/json; charset=UTF-8');
-  ctx.body = body;
+  // First try and get layout json from the server,
+  // if it fails then use the local layout json file
+  try {
+    console.log(layoutBaseUrl + reqPath);
+    const response = await fetch(layoutBaseUrl + reqPath,
+      {
+        method: 'GET',
+        headers: ctx.headers,
+        agent
+      }
+    );
+
+    const data = await response.json();
+    console.log('yo yo', data);
+    ctx.set('Content-Type', 'application/json; charset=UTF-8');
+    ctx.body = data;
+  }
+  catch (ex) {
+    console.log('here');
+    try {
+      const body = await readFileThunk(filePath);
+      ctx.set('Content-Type', 'application/json; charset=UTF-8');
+      ctx.body = body;
+    }
+    catch (ex) {
+      console.log('File loading failed');
+    }
+  }
 }
 
