6a2fe915ec16b7828cf1f0ea3da2d503a8c7643e
webdev498 <gcmobile@mail.com>
2016-12-12 18:02:16 +0530

    updated kibana tests

--- tests/layouts/CoreLayout/Search.spec.js
+++ tests/layouts/CoreLayout/Search.spec.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-
-import {wrapThemeProvider} from '../testUtils';
-
 import {Search} from 'layouts/CoreLayout/Search';
 
 describe('<Search />', () => {
--- tests/utils/kibanaUtils.spec.js
+++ tests/utils/kibanaUtils.spec.js
@@ -1,16 +1,49 @@
 import {
-  generatePathParams,
-  generateQueryParams
-  } from 'utils/kibanaUtils';
+  generateClickThroughUrl
+} from 'utils/kibanaUtils';
+
+const kibanaBaseUrl = (window.global && window.global.kibanaBaseUrl) ? window.global.kibanaBaseUrl : '/';
 
 describe('Kibana Utility Function:', function() {
-  it('generatePathParams should return path parameters.', function() {
-    expect(generatePathParams(['alerts-score'])).to.deep.equal('alerts-score');
-  });
-
-  it('generateQueryParams should return query parameters.', function() {
-    const parameters = {"data":{"total":-1,"next":-1,"columns":[{"name":"data.rank_alert.category","displayName":"Category","columnType":"DIMENSION","dataType":"TEXT","sortable":true},{"name":"date","displayName":"count of alerts","columnType":"MEASURE","dataType":"NUMBER","sortable":true}],"rows":[["suspicious-login",9],["potential-apt",2],["bad-reputation-traffic",1]]},"duration":"1h","dataObj":{"chartX":154,"chartY":217,"pageX":292,"pageY":948,"index":0,"value":9,"displayValue":"9","toolText":"suspicious-login, 9","id":"","datasetIndex":3,"visible":true,"dataIndex":0,"dataValue":9},"queryParamsArray":{"window":"","type":""}},
-      queryParams = '?window=1h&type=suspicious-login';
-    expect(generateQueryParams(parameters)).to.deep.equal(queryParams);
+  it('should return kibana url', function() {
+    const parameters = {
+        'data': {
+          'columns': [
+            {
+              'name': 'data.rank_alert.category',
+              'displayName': 'Category',
+              'columnType': 'DIMENSION',
+              'dataType': 'TEXT',
+              'sortable': true
+            },
+            {
+              'name': 'date',
+              'displayName': 'count of alerts',
+              'columnType': 'MEASURE',
+              'dataType': 'NUMBER',
+              'sortable': true
+            }
+          ],
+          'rows': [
+            [
+              'bad-reputation-traffic',
+              354
+            ]
+          ]
+        },
+        'duration': '1mo',
+        'dataObj': {
+          'toolText': 'bad-reputation-traffic, 42.45%'
+        },
+        'queryParamsArray': {
+          'window': '',
+          'xAxisValueInLowerCase': 'type'
+        },
+        'pathParams': [
+          'alerts-type'
+        ]
+      },
+      url = kibanaBaseUrl + '/api/kibana/query/alerts-type?window=1mo&type=bad-reputation-traffic';
+    expect(generateClickThroughUrl(parameters)).to.deep.equal(url);
   });
 });
