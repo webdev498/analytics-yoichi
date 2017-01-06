10ba8416aa7667ba428c9dea92fc99da2025fa8e
webdev498 <gcmobile@mail.com>
2017-01-06 16:52:36 +0530

    Adds tests for fetchLayoutData function for corejs actions

--- tests/client/actions/core.spec.js
+++ tests/client/actions/core.spec.js
@@ -97,7 +97,7 @@ describe('corelayout Redux Actions', () => {
   context('fetchLayoutData function', function() {
     let server, auth, store;
 
-    const json = {__authDetails: {}, taf: {}},
+    const json = {layout: []},
       jsonRes = JSON.stringify(json);
 
     beforeEach(function() {
@@ -111,25 +111,65 @@ describe('corelayout Redux Actions', () => {
     });
 
     it('should dispatch RECEIVE_LAYOUT_DATA with json data', () => {
-      // server.respondWith('GET', [ 200, { 'Content-Type': 'application/json' }, jsonRes ]);
-      // const apiObj = store.dispatch(fetchUserData())
-      //   .then(res => {
-      //     const actions = store.getActions();
-      //     expect(actions).to.have.length(2);
-      //     expect(actions[0]).to.have.a.property('type', USER_DETAILS_LOADING);
+      const id = '/';
+      server.respondWith('GET', defaultLayoutPath, [ 200, { 'Content-Type': 'application/json' }, jsonRes ]);
+      const apiObj = store.dispatch(fetchLayoutData(id))
+        .then(res => {
+          const actions = store.getActions();
+          expect(actions).to.have.length(2);
+          expect(actions[0]).to.have.a.property('type', REQUEST_LAYOUT_DATA);
+          expect(actions[0]).to.have.a.property('id', id);
 
-      //     expect(actions[1]).to.have.a.property('type', USER_DETAILS_LOADED);
-      //     expect(actions[1]).to.have.a.property('data');
-      //     expect(actions[1].data).to.deep.equal(json);
-      //   });
+          expect(actions[1]).to.have.a.property('type', RECEIVE_LAYOUT_DATA);
+          expect(actions[1]).to.have.a.property('id', id);
+          expect(actions[1]).to.have.a.property('data');
+          expect(actions[1].data).to.deep.equal({json});
+        });
 
-      // server.respond();
-      // return apiObj;
+      server.respond();
+      return apiObj;
     });
 
-    it('should dispatch ERROR_LAYOUT_DATA with error "Json not loaded correctly from server"');
-    it('should dispatch ERROR_LAYOUT_DATA with error server error');
+    it('should dispatch ERROR_LAYOUT_DATA with error "Json not loaded correctly from server"', () => {
+      const id = '/';
+      server.respondWith('GET', defaultLayoutPath, [ 200, { 'Content-Type': 'application/json' }, '' ]);
+      const apiObj = store.dispatch(fetchLayoutData(id))
+        .then(res => {
+          const actions = store.getActions();
+          expect(actions).to.have.length(2);
+          expect(actions[0]).to.have.a.property('type', REQUEST_LAYOUT_DATA);
+          expect(actions[0]).to.have.a.property('id', id);
+
+          expect(actions[1]).to.have.a.property('type', ERROR_LAYOUT_DATA);
+          expect(actions[1]).to.have.a.property('id', id);
+          expect(actions[1]).to.have.a.property('errorData');
+        });
+
+      server.respond();
+      return apiObj;
+    });
+
+    it('should dispatch ERROR_LAYOUT_DATA with error server error', () => {
+      const id = '/';
+      server.respondWith('GET', defaultLayoutPath, [ 400, { 'Content-Type': 'application/json' }, jsonRes ]);
+      const apiObj = store.dispatch(fetchLayoutData(id))
+        .then(res => {
+          const actions = store.getActions();
+          expect(actions).to.have.length(2);
+          expect(actions[0]).to.have.a.property('type', REQUEST_LAYOUT_DATA);
+          expect(actions[0]).to.have.a.property('id', id);
+
+          expect(actions[1]).to.have.a.property('type', ERROR_LAYOUT_DATA);
+          expect(actions[1]).to.have.a.property('id', id);
+          expect(actions[1]).to.have.a.property('errorData');
+        });
+
+      server.respond();
+      return apiObj;
+    });
   });
 
-  // context('fetchSearchData function');
+  // context('fetchSearchData function', function() {
+
+  // });
 });
