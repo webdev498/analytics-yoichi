e1e07bf37622b5a8afa15ad07358775e9823a7bf
webdev498 <gcmobile@mail.com>
2017-01-16 16:27:51 +0530

    added more tests for pagination widget component

--- tests/client/components/widgets/PaginationWidget.spec.js
+++ tests/client/components/widgets/PaginationWidget.spec.js
@@ -12,7 +12,7 @@ let props = {
   id: 'pagination',
   size: 20,
   currentPage: 1,
-  maxNumbersOnLeftRight: 4,
+  maxNumbersOnLeftRight: 5,
   fetchData: spy(),
   onPrevPageChanged: spy(),
   type: 'primary',
@@ -25,10 +25,6 @@ function renderPaginationWidget() {
 }
 
 function mountPaginationWidget() {
-  return mount(<PaginationWidget {...props} />);
-}
-
-function mountPaginationWidgetComponent() {
   let component = mount(<PaginationWidget {...props} />);
   return component.find('PaginationWidget');
 }
@@ -48,45 +44,74 @@ describe('<PaginationWidget />', () => {
     expect(component.props().currentPage).to.equal(1);
     expect(component.props().maxNumbersOnLeftRight).to.be.defined;
     expect(component.props().maxNumbersOnLeftRight).to.be.an('number');
-    expect(component.props().maxNumbersOnLeftRight).to.equal(4);
+    expect(component.props().maxNumbersOnLeftRight).to.equal(5);
     expect(component.type()).to.equal(PaginationWidget);
     expect(component.props().type).to.be.defined;
     expect(component.props().type).to.be.an('string');
+    expect(component.props().type).to.equal('primary');
     expect(component.props().style).to.be.defined;
     expect(component.props().style).to.be.an('object');
     expect(component.props().fetchData).to.exist;
     expect(component.props().fetchData).to.be.a('function');
   });
 
-  it('should display pagination links correctly', () => {
+  it('should display pagination buttons correctly', () => {
     const component = renderPaginationWidget(),
       size = component.props().size,
       currentPage = component.props().currentPage,
       maxNumbersOnLeftRight = component.props().maxNumbersOnLeftRight,
-      pageLimits = {
-        start: 1,
-        end: 9
-      };
-    expect(getPaginationButtonsLimits(size, currentPage, maxNumbersOnLeftRight)).to.deep.equal(pageLimits);
+      limits = getPaginationButtonsLimits(size, currentPage, maxNumbersOnLeftRight);
+    expect(limits).to.be.an('object');
+    expect(limits.start).to.equal(1);
+    expect(limits.end).to.equal(11);
   });
 
-  it('should click pagination link', () => {
-    const component = mountPaginationWidgetComponent();
-    const paginationLink = component.find('.prev-pagination-link');
-    paginationLink.simulate('click');
-    expect(component.props().fetchData.callCount).to.equal(1);
+  it('should have <ul> and <li> elements', () => {
+    const component = mountPaginationWidget();
+    expect(component.find('ul')).to.have.length(1);
+    expect(component.find('ul').children()).to.have.length(13);
+    expect(component.find('ul').childAt(0).type()).to.equal('li');
+    expect(component.find('li')).to.have.length(13);
+  });
+
+  it('each <li> should have one <button> element', () => {
+    const component = mountPaginationWidget();
+    expect(component.find('ul').childAt(0).childAt(0).type()).to.equal('button');
+  });
+
+  it('all <li> should have key', () => {
+    const component = mountPaginationWidget();
+    expect(component.find('ul').childAt(0).key()).to.be.defined;
   });
 
   it('simulates page buttons click events', () => {
-    const onButtonClick = sinon.spy();
-    const wrapper = mount(
-      <PaginationWidget {...props}>
-        <ul className='pagination'>
-          <li key='prev'><button className='prev-pagination-link' onClick={onButtonClick()}>&lt;&lt;</button></li>
-        </ul>
-      </PaginationWidget>
-    );
+    const onPrevPageChanged = sinon.spy(),
+      onNextPageChanged = sinon.spy(),
+      wrapper = mount(
+        <PaginationWidget {...props}>
+          <ul className='pagination'>
+            <li key='Prev'><button className='prev-pagination-link' onClick={onPrevPageChanged()}>&lt;&lt;</button></li>
+            <li key='Next'><button className='next-pagination-link' onClick={onNextPageChanged()}>&gt;&gt;</button></li>
+          </ul>
+        </PaginationWidget>
+      );
     wrapper.find('.prev-pagination-link').simulate('click');
-    expect(onButtonClick.calledOnce).to.equal(true);
+    expect(onPrevPageChanged.calledOnce).to.equal(true);
+    expect(onPrevPageChanged.callCount).to.equal(1);
+
+    wrapper.find('.next-pagination-link').simulate('click');
+    expect(onNextPageChanged.calledOnce).to.equal(true);
+    expect(onNextPageChanged.callCount).to.equal(1);
+  });
+
+  it('should have active class for the current page button', () => {
+    const component = mountPaginationWidget();
+    const currentPageLink = component.find('.active');
+    expect(currentPageLink.key()).to.equal('1');
+  });
+
+  it('should have active class only for one <li>', () => {
+    const component = mountPaginationWidget();
+    expect(component.find('.active')).to.have.length(1);
   });
 });
