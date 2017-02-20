772ffcb7c2178126b727f64e471c912093a1d351
webdev498 <gcmobile@mail.com>
2017-02-20 17:46:53 +0530

    Updates color palette for summary page

--- commons/colors.js
+++ commons/colors.js
@@ -19,11 +19,30 @@ export const BlackColor = '#000000';
 export const NavigationColor = 'rgba(255, 255, 255, 0.54)';
 export const SliderColor = 'rgba(36,41,61, 0.25)';
 
+export const BarColor = '#03c5ed';
+export const BarColorShade1 = '#1bcbef';
+export const BarColorShade2 = '#35d1f1';
+export const BarColorShade3 = '#4ed6f2';
+export const BarColorShade4 = '#6adff7';
+export const BarColorShade5 = '#7fe2f6';
+export const BarColorShade6 = '#99e9f9';
+export const BarColorShade7 = '#adecf9';
+export const BarColorShade8 = '#d4f5fc';
+export const BarColorShade9 = '#eafbfe';
+
+export const OrangeColor = '#f3763a';
+export const OrangeColorShade1 = '#f58854';
+export const OrangeColorShade2 = '#f79e73';
+export const OrangeColorShade3 = '#f9b696';
+export const OrangeColorShade4 = '#fbd1bd';
+export const OrangeColorShade5 = '#fde7dc';
+export const OrangeColorShade6 = '#fef5f1';
+
 export const SearchColor = '#363E51';
 
 export const AreaSparkLineChartColor = {
-  'area': '#BFEFEE',
-  'border': '#59DED9'
+  area: '#adecf9',
+  border: '#03c5ed'
 };
 
 export const DurationWidgetColor = {
@@ -42,12 +61,12 @@ export const WorldmapLegendsColor = {
     '#DBF8F7'
   ],
   'malicious': [
-    CoralColor,
-    '#F7A48B',
-    '#F9B6A2',
-    '#F8CABB',
-    '#FCDBD2',
-    '#FEEDE8'
+    OrangeColor,
+    OrangeColorShade1,
+    OrangeColorShade2,
+    OrangeColorShade3,
+    OrangeColorShade4,
+    OrangeColorShade5
   ]
 };
 
@@ -55,8 +74,9 @@ export const SubHeadingBGColor = '#F7F7F9';
 
 export const AxisColor = '#DADADA';
 
-export const DefaultGraphPaletteColors =
-  '#2bd8d0,#3ad7c9,#46d6c4,#57d5bd,#67d3b6,#79d2ae,#8ad1a7,#9acfa0,#a8ce9a,#b2cd96';
+export const DefaultGraphPaletteColors = `${BarColor},${BarColorShade1},${BarColorShade2},\
+${BarColorShade3},${BarColorShade4},${BarColorShade5},${BarColorShade6},${BarColorShade7},\
+${BarColorShade8},${BarColorShade9}`;
 
 export const TimelineBarColors = [
   '#2AD8D0',
@@ -107,7 +127,8 @@ export const Colors = {
   selectedActionBG: selectedActionBGColor,
   notificationMessageBG: notificationMessageBGColor,
   cloudShades: CloudColorShades,
-  search: SearchColor
+  search: SearchColor,
+  bar: BarColor
 };
 
 export const PrimaryPalette = {
