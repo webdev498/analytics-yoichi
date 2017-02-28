c7750c8693e47b46511c2696f293072738436a80
webdev498 <gcmobile@mail.com>
2017-02-28 18:38:42 +0530

    fixed timeline traffic filter issue

---
+++ tests/client/containers/SearchBar.spec.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { mount, shallow } from 'enzyme';
+import {spy} from 'sinon';
+
+import SearchBar from 'containers/SearchBar';
+import FontIcon from 'material-ui/FontIcon';
+import {wrapThemeProvider} from '../../testUtils';
+
+function shallowRenderSearchBar(propsOptions) {
+  let props = {
+    floatingSearchBar: true,
+    loadFloatingSearchBar: spy(),
+    updateSearch: spy(),
+    searchText: ''
+  };
+
+  props = Object.assign({}, props, propsOptions);
+
+  let component = shallow(wrapThemeProvider(<SearchBar {...props} />));
+  return component.find('SearchBar');
+}
+
+function mountSearchBar(propsOptions) {
+  let props = {
+    floatingSearchBar: true,
+    loadFloatingSearchBar: spy(),
+    updateSearch: spy(),
+    searchText: ''
+  };
+
+  props = Object.assign({}, props, propsOptions);
+
+  // let component = mount(wrapThemeProvider(<SearchBar {...props} />));
+  // return component;
+  let component = mount(<SearchBar {...props} />);
+  return component;
+  // let component = mount(wrapThemeProvider(<SearchBar {...props} />));
+  // return component.find('SearchBar');
+}
+
+describe('<SearchBar />', () => {
+  it('exists', () => {
+    expect(SearchBar).to.exist;
+  });
+
+  it('has correct props', () => {
+    const component = shallowRenderSearchBar({floatingSearchBar: true, searchText: 'malware'});
+
+    expect(component.props().floatingSearchBar).to.equal(true);
+    expect(component.props().loadFloatingSearchBar).to.exist;
+    expect(component.props().loadFloatingSearchBar).to.be.a('function');
+    expect(component.props().updateSearch).to.exist;
+    expect(component.props().updateSearch).to.be.a('function');
+    expect(component.props().searchText).to.equal('malware');
+  });
+
+  it('should display clear icon if search bar is floating', () => {
+    const component = mountSearchBar({floatingSearchBar: true});
+    console.log(component.state());
+    expect(component.state().showClearIcon).to.be.defined;
+    expect(component.state().showClearIcon).to.equal(true);
+  });
+});
